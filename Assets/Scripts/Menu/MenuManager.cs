f41fd474af36531417f9b07208ffcf154ea47f92
Marcela <maa5@cin.ufpe.br>
2025-03-23 23:49:56 -0300

    feat: adds menu options to gui

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -8,6 +8,22 @@ namespace Brawlley
         #region Data
         [Header("Menu Data")]
         Stack<MenuPanel> menuStack = new();
+
+        [Header("Menu Panels")]
+        [SerializeField] private GameObject gameModePanel;
+        [SerializeField] private GameObject selectMapPanel;
+
+        private int selectedMode = -1;
+        private LoadScene loadScene;
+        #endregion
+
+        #region Unity Methods
+        void Start()
+        {
+            gameModePanel.SetActive(true);
+            selectMapPanel.SetActive(false);
+            loadScene = FindObjectOfType<LoadScene>(); // Gets the reference to LoadScene
+        }
         #endregion
 
         #region Menu Methods
@@ -22,6 +38,23 @@ namespace Brawlley
             }
         }
         public void QuitGame() => Application.Quit();
+        #endregion
+
+        #region Game Mode Selection
+        public void SelectMode(int mode)
+        {
+            selectedMode = mode;
+            gameModePanel.SetActive(false);
+            selectMapPanel.SetActive(true);
+        }
+
+        public void SelectMap(int sceneIndex)
+        {
+            if (selectedMode != -1 && loadScene != null)
+            {
+                loadScene.LoadSceneByIndex(sceneIndex);
+            }
+        }
         #endregion
     }
 }
